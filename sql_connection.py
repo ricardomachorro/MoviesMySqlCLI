17ba5cce488e69f0fb5bf8fd30f868ec3c2c1792
ricardomachorro <ricardoalbeto.machorrovences@gmail.com>
2025-04-01 22:05:04 -0600

    Sql script for movie table and function for select movies

--- sql_connection.py
+++ sql_connection.py
@@ -3,7 +3,19 @@ import mysql.connector
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
-  password="Ramv1357"
+  password="Ramv1357",
+  database ="pythonmysqlmovieticketsys"
 )
 
-print(mydb)
+
+
+def select_movies():
+    mycursor = mydb.cursor()
+    mycursor.execute("Select * from Movies")
+    myresult = mycursor.fetchall()
+
+    for movie in myresult:
+        print("Id:"+str(movie[0]) +" , title:"+ movie[1])
+
+
+select_movies()
